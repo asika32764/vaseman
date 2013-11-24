b238937c8deab0e71f6cafcc3faea1ab41a654ca
Asika <asika32764@gmail.com>
2013-11-24 21:37:46 +0800

    Add Joomla Console

--- src/Helper/Example.php
+++ src/Helper/Example.php
@@ -4,10 +4,9 @@ namespace Helper;
 
 use DI\BaseHelper as Helper;
 
-class Database extends Helper
+class Example extends Helper
 {
     public $databases = array('課程大綱', '博碩士論文資訊網', '報紙資料庫', '雜誌資料庫', '維基百科', 'Webometrics');
-
 
 	public function getDatabases()
 	{
